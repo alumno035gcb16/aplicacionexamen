b1e5ac1a89dc5129a5d123619567b300aaebd623
alumno035gcb16 <bukiarchundia@gmail.com>
2014-11-05 13:53:12 -0600

    error3
    creo que ya

--- jquery-mobile/acciones.js
+++ jquery-mobile/acciones.js
@@ -21,6 +21,6 @@ $(document).ready(function(e) {
 		},false);
 });
 function escribehistoria(accion){
-	$('#eHistoria').append('<li>'+accino+'</li>');
+	$('#eHistoria').append('<li>'+accion+'</li>');
 }
 
